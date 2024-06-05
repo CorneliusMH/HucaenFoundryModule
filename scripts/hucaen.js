94efa78f7b432e4b5c6b585eb95fcb71eda37214
CorneliusMH <CorneliusMH@users.noreply.github.com>
2024-06-04 22:57:44 -0400

    move packs to src/packs

--- scripts/hucaen.js
+++ scripts/hucaen.js
@@ -1,1 +1,17 @@
 console.log('todo-list | Hello World!');
+
+import { compilePack } from "@foundryvtt/foundryvtt-cli";
+import { promises as fs } from "fs";
+import path from "path";
+
+const packs = await fs.readdir(path.join("src", "packs"));
+for (const pack of packs) {
+  if (pack === ".gitattributes") continue;
+  console.log("Packing " + pack);
+  const src = path.join("src", "packs", pack);
+  const dest = path.join("packs", pack);
+  await compilePack(src, dest, {
+    json: true,
+    log: true
+  });
+}
