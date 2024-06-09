c725a624f35cf29620f13c66b5ca59bb78e0488c
CorneliusMH <CorneliusMH@users.noreply.github.com>
2024-06-09 00:18:21 -0400

    Let's give this a whirl

--- scripts/hucaen.js
+++ scripts/hucaen.js
@@ -1,16 +1,16 @@
 console.log('Hucaen Module | Hello World!');
 
-// import { compilePack } from "@foundryvtt/foundryvtt-cli";
-// import { promises as fs } from "fs";
-// import path from "path";
+import { compilePack } from "@foundryvtt/foundryvtt-cli";
+import { promises as fs } from "fs";
+import path from "path";
 
-// const packs = await fs.readdir(path.join("src", "packs"));
-// for (const pack of packs) {
-//   if (pack === ".gitattributes") continue;
-//   console.log("Packing " + pack);
-//   const src = path.join("src", "packs", pack);
-//   const dest = path.join("packs", pack);
-//   await compilePack(src, dest, {
-//     log: true
-//   });
-// }
+const packs = await fs.readdir(path.join("src", "packs"));
+for (const pack of packs) {
+  if (pack === ".gitattributes") continue;
+  console.log("Hucaen Module | Packing " + pack);
+  const src = path.join("src", "packs", pack);
+  const dest = path.join("packs", pack);
+  await compilePack(src, dest, {
+    log: true
+  });
+}
